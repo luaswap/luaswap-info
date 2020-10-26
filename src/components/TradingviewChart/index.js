f026a3849dfcee3c4fe8e26a087ddabc458166d9
thanhvk <thanhvk.w3s@gmail.com>
2020-10-26 16:20:43 +0700

    change fee to 0.5%

--- src/components/TradingviewChart/index.js
+++ src/components/TradingviewChart/index.js
@@ -163,7 +163,7 @@ const TradingViewChart = ({
 
       // format numbers
       let percentChange = baseChange?.toFixed(2)
-      let formattedPercentChange = (percentChange > 0 ? '+' : '') + percentChange + '%'
+      let formattedPercentChange = percentChange ? (percentChange > 0 ? '+' : '') + percentChange + '%' : ''
       let color = percentChange >= 0 ? 'green' : 'red'
 
       // get the title of the chart
