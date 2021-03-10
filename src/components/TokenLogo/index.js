423fbe3da2c75eba0c50212a590d9812492af2bb
PhucNH <phucnh.dev@gmail.com>
2021-03-10 13:53:35 +0700

    Update new version token list icon

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -67,7 +67,7 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
     )
   }
 
-  const path = `https://raw.githubusercontent.com/tomochain/luaswap-token-list/713175a8891d4597a289a70545be9dbe07822106/src/tokens/icons/tomochain/${isAddress(
+  const path = `https://raw.githubusercontent.com/tomochain/luaswap-token-list/7b3a68fdd3dbf7536795d030e695403950959a3a/src/tokens/icons/tomochain/${isAddress(
     address
   )}.png`
 
