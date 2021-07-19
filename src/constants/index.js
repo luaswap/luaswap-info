b5b4f2e4e153b174a9918d537bf6c363a42736d8
PhucNH <phucnh.dev@gmail.com>
2021-07-19 15:25:21 +0700

    Upgrade new token

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,7 +14,7 @@ export const timeframeOptions = {
 export const SUPPORTED_LIST_URLS__NO_ENS = [
   'https://gateway.ipfs.io/ipns/tokens.uniswap.org',
   'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
-  'https://raw.githubusercontent.com/tomochain/luaswap-token-list/ed8f53056e2935ff84a8344699730904b431b1ca/build/luaswap-default.tokenlist.json'
+  'https://raw.githubusercontent.com/tomochain/luaswap-token-list/7ab366e49618c07ec8b9c39c1f93c4e58e1429fb/build/luaswap-default.tokenlist.json'
 ]
 
 // hide from overview list
