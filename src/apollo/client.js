d4a032cacb807266a48abf8a9a70dccf629893db
Huu Phuc Nguyen <phucnh@Huus-MacBook-Pro.local>
2022-09-19 09:29:54 +0700

    change api check status

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -16,7 +16,7 @@ export const client = new ApolloClient({
 export const healthClient = new ApolloClient({
   link: new HttpLink({
     // uri: 'https://api.thegraph.com/index-node/graphql' // Hosted services subgraph
-    uri: 'https://suntzu.me/status'
+    uri: 'https://graph-mainnet.dapplooker.com/status'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
