f135575936eb2537d57c1c9c86d04b137e96960b
Huu Phuc Nguyen <phucnh@Huus-MacBook-Pro.local>
2022-10-10 15:41:24 +0700

    Change status api url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -16,7 +16,7 @@ export const client = new ApolloClient({
 export const healthClient = new ApolloClient({
   link: new HttpLink({
     // uri: 'https://api.thegraph.com/index-node/graphql' // Hosted services subgraph
-    uri: 'https://graph-mainnet.dapplooker.com/status'
+    uri: 'https://graph-indexer.prod-us-west-2.staked.cloud/status'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
